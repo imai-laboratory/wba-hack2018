f4fb512e2a7e7a0b35546f6de4efb43b0843bc00
yukia18 <abe@ailab.ics.keio.ac.jp>
2018-10-05 16:34:31 +0900

    MOD: docker for gpu

--- oculomotor/helpers/interactive.sh
+++ oculomotor/helpers/interactive.sh
@@ -3,4 +3,4 @@
 LOCAL_APP=${PWD}/application
 CONTAINER_APP=/opt/oculomotor
 
-docker run -it -p 5000:5000 --rm -v ${PWD}:${CONTAINER_APP} wbap/oculomotor
+nvidia-docker run -it -p 5000:5000 --rm -v ${PWD}:${CONTAINER_APP} wbap/oculomotor
