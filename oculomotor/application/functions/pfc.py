f06b4fca796b04bfed8388993dfc6a0fa6ac2c9d
takuseno <takuma.seno@gmail.com>
2018-10-07 19:30:05 +0900

    Remove cursorfindaccumulator

--- oculomotor/application/functions/pfc.py
+++ oculomotor/application/functions/pfc.py
@@ -18,32 +18,6 @@ class Phase(object):
     START = 0  # Start phase while finding red cross cursor
     TARGET = 1 # Target finding phsae
 
-
-class CursorFindAccumulator(object):
-    def __init__(self, decay_rate=0.9):
-        # Accumulated likelilood
-        self.decay_rate = decay_rate
-        self.likelihood = 0.0
-
-        self.cursor_template = load_image("data/debug_cursor_template_w.png")
-
-    def accumulate(self, value):
-        self.likelihood += value
-        self.likelihood = np.clip(self.likelihood, 0.0, 1.0)
-
-    def reset(self):
-        self.likelihood = 0.0
-
-    def process(self, retina_image):
-        match = cv2.matchTemplate(retina_image, self.cursor_template,
-                                  cv2.TM_CCOEFF_NORMED)
-        match_rate = np.max(match)
-        self.accumulate(match_rate * 0.1)
-
-    def post_process(self):
-        # Decay likelihood
-        self.likelihood *= self.decay_rate
-
 # Accumulator Based Arbitration Model
 # https://link.springer.com/chapter/10.1007/978-3-319-70087-8_63
 # we proposed arbitration model based on prefrontal cortex
@@ -82,7 +56,6 @@ class PFC(object):
         self.timing = brica.Timing(3, 1, 0)
 
         self.vae_cursor_accumulator = Abam(len(MODEL_PATHS.keys()))
-        self.cursor_find_accmulator = CursorFindAccumulator()
         # replace cursor_find_accmulator with vae error accumulator
         # if vae is not consistently reliable, the scene is finding cursor
         self.vae_error_accumulators = {}
@@ -108,10 +81,6 @@ class PFC(object):
         # Allocentrix map image from hippocampal formatin module.
         map_image = inputs['from_hp']
 
-        # This is a very sample implementation of phase detection.
-        # You should change here as you like.
-        self.cursor_find_accmulator.process(retina_image)
-        self.cursor_find_accmulator.post_process()
         bg_message = 0
         bg_findcursor = 0
 
