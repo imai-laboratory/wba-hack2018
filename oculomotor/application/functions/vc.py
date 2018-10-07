5ca1b1a061c9c0e4a949a6a49f5ccfc0e3773a89
takuseno <takuma.seno@gmail.com>
2018-10-07 18:49:06 +0900

    Refactor vc

--- oculomotor/application/functions/vc.py
+++ oculomotor/application/functions/vc.py
@@ -19,44 +19,39 @@ class VC(object):
     """
     def __init__(self, skip=False):
         self.timing = brica.Timing(2, 1, 0)
-        if skip:
-            self.model = None
-        else:
-            print('loading beta VAE')
-            # input_shape: (224, 224, 3)
-            # self.model = K.applications.vgg16.VGG16()
-            # self.model = K.applications.mobilenet.MobileNet()
-            tensors = OrderedDict()
-            for i, (name, path) in enumerate(MODEL_PATHS.items()):
-                tensor = build(constants, name)
-                tensors[name] = tensor
-            self.reconst, self.generate = self.build_tf_call(tensors)
 
-            config = tf.ConfigProto(
-                device_count={'GPU': 1}  # NO GPU
-            )
-            config.gpu_options.allow_growth = True
+        # build vae graphs on each task
+        tensors = OrderedDict()
+        for i, (name, path) in enumerate(MODEL_PATHS.items()):
+            tensor = build(constants, name)
+            tensors[name] = tensor
+        # aggregate all tensors in single inference
+        self.reconst, self.generate = self.build_tf_call(tensors)
 
-            self.sess = tf.Session(config=config)
+        config = tf.ConfigProto()
+        config.gpu_options.allow_growth = True
+        self.sess = tf.Session(config=config)
 
-            # create savers
-            savers = {}
-            for name in MODEL_PATHS.keys():
-                variables = tf.get_collection(
-                    tf.GraphKeys.TRAINABLE_VARIABLES, name)
-                var_list = {}
-                for var in variables:
-                    key = var.name
-                    var_list[key.replace(name, 'vae')[:-2]] = var
-                savers[name] = tf.train.Saver(var_list)
+        # create savers on each graph
+        savers = {}
+        for name in MODEL_PATHS.keys():
+            variables = tf.get_collection(
+                tf.GraphKeys.TRAINABLE_VARIABLES, name)
+            var_list = {}
+            for var in variables:
+                key = var.name
+                # replace vae in namespace with task name
+                var_list[key.replace(name, 'vae')[:-2]] = var
+            savers[name] = tf.train.Saver(var_list)
 
-            self.sess.__enter__()
-            self.sess.run(tf.global_variables_initializer())
+        self.sess.__enter__()
+        self.sess.run(tf.global_variables_initializer())
 
-            # load all saved models
-            for name, saver in savers.items():
-                saver.restore(self.sess, MODEL_PATHS[name])
-        self.skip = skip
+        print('loading beta VAE')
+        # load all saved models
+        for name, saver in savers.items():
+            saver.restore(self.sess, MODEL_PATHS[name])
+
         self.last_vae_reconstruction = None
         self.last_vae_top_errors = None
 
@@ -70,58 +65,55 @@ class VC(object):
             raise Exception('VC did not recieve from Retina')
 
         retina_image = inputs['from_retina']
-        # TODO(->smatsumori): add pretrained models
-        # todo(->smatsumori): check inputs shape
-
 
         # feature extraction
-        # TODO: remove skip
-        if self.skip:
-            processed_images = (retina_image, None)
-        else:
-            # resizing image
-            reshape_size = (64, 64, 3)
-            reshaped_image = sp.misc.imresize(
-                retina_image, reshape_size, interp='bilinear')
+        # resizing image
+        reshape_size = (64, 64, 3)
+        reshaped_image = sp.misc.imresize(
+            retina_image, reshape_size, interp='bilinear')
 
-            # VAE reconstruction and get latent parameters
-            input_image = np.array(reshaped_image, dtype=np.float32) / 255.0
-            with self.sess.as_default():
-                # WARN: using magi no
-                # reconstructed.len: 12
-                reconstructed = self.reconst([input_image])
+        # VAE reconstruction and get latent parameters
+        input_image = np.array(reshaped_image, dtype=np.float32) / 255.0
+        with self.sess.as_default():
+            # WARN: using magi no
+            # reconstructed.len: 12
+            reconstructed = self.reconst([input_image])
 
-                # recont_images.shape: (6, 128, 128, 3)
-                # latents.shape: (6, 8)
-                recont_images, latents = reconstructed[:6], reconstructed[6:]
-                images = OrderedDict()
-                pixel_errors = OrderedDict()
-                top_errors = OrderedDict()
-                dc_latents = {}
-                for image, latent, name\
-                        in zip(recont_images, latents, MODEL_PATHS.keys()):
-                    images[name] = image[0]
-                    pixel_error = (image[0] - input_image) ** 2
-                    pixel_errors[name] = pixel_error
-                    # top 10% errors
-                    flatten = np.reshape(pixel_error, [-1])
-                    size = int(flatten.shape[0] * 0.01)
-                    max_indices = np.argpartition(-flatten, size)[:size]
-                    top_error = np.zeros(flatten.shape, dtype=np.float32)
-                    top_error[max_indices] = flatten[max_indices]
-                    top_error = np.reshape(top_error, pixel_error.shape)
-                    # flatten channel
-                    channel_mean_top_error = top_error.mean(-1)
-                    flatten_mean_error = np.reshape(channel_mean_top_error, [-1])
-                    flatten_mean_error[flatten_mean_error > 0] = softmax(flatten_mean_error[flatten_mean_error > 0])
-                    flatten_mean_error *= 1.0 / (np.max(flatten_mean_error) + 1e-5)
-                    flatten_mean_error[np.isnan(flatten_mean_error)] = 0.0
-                    top_error = np.reshape(flatten_mean_error, pixel_error.shape[:-1])
-                    top_error = np.array(top_error * 255.0, dtype=np.uint8)
-                    top_error = cv2.resize(top_error, (128, 128))
+            # recont_images.shape: (6, 64, 64, 3)
+            # latents.shape: (6, 8)
+            recont_images, latents = reconstructed[:6], reconstructed[6:]
+            images = OrderedDict()
+            pixel_errors = OrderedDict()
+            top_errors = OrderedDict()
+            dc_latents = {}
+            for image, latent, name\
+                    in zip(recont_images, latents, MODEL_PATHS.keys()):
+                images[name] = image[0]
+                pixel_error = (image[0] - input_image) ** 2
+                pixel_errors[name] = pixel_error
+                # top 10% errors
+                flatten = np.reshape(pixel_error, [-1])
+                size = int(flatten.shape[0] * 0.01)
+                max_indices = np.argpartition(-flatten, size)[:size]
+                top_error = np.zeros(flatten.shape, dtype=np.float32)
+                top_error[max_indices] = flatten[max_indices]
+                # mean RGB channel
+                top_error = np.reshape(top_error, pixel_error.shape)
+                channel_mean_top_error = top_error.mean(-1)
+                # flatten errors to normalize
+                flatten_mean_error = np.reshape(channel_mean_top_error, [-1])
+                flatten_mean_error[flatten_mean_error > 0] = softmax(
+                    flatten_mean_error[flatten_mean_error > 0])
+                flatten_mean_error *= 1.0 / (np.max(flatten_mean_error) + 1e-5)
+                flatten_mean_error[np.isnan(flatten_mean_error)] = 0.0
+                # reshape to (64, 64)
+                top_error = np.reshape(flatten_mean_error, pixel_error.shape[:-1])
+                # multiply 255 to correctly resize error as an image
+                top_error = np.array(top_error * 255.0, dtype=np.uint8)
+                top_error = cv2.resize(top_error, (128, 128))
+                top_errors[name] = np.array(top_error, dtype=np.float32) / 255.0
 
-                    top_errors[name] = np.array(top_error, dtype=np.float32) / 255.0
-                    dc_latents[name] = latent[0]
+                dc_latents[name] = latent[0]
 
             to_fef = (retina_image, pixel_errors, top_errors, dc_latents)
             to_pfc = (retina_image, pixel_errors, top_errors)
@@ -133,7 +125,9 @@ class VC(object):
         # TODO: change pfc fef
         return dict(to_fef=to_fef, to_pfc=to_pfc, to_hp=to_hp)
 
+    # aggregate function to infere in a single sesion.run
     def build_tf_call(self, tensors):
+        # reconstruct input image
         def reconstruct(inputs):
             feed_dict = {}
             ops = []
@@ -154,6 +148,7 @@ class VC(object):
             sess = tf.get_default_session()
             return sess.run(ops, feed_dict)
 
+        # generate image from latent variable
         def generate(latent):
             feed_dict = {}
             ops = []
